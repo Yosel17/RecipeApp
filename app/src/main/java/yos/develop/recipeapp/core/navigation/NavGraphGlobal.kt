f04a1db71ffa62eb6d9ea6574fa586d302adf6fb
Yosel Alvizures C <Yosel11517@gmail.com>
2025-02-14 08:26:23 -0600

    Se agrega animaciones personalizadas en la navegacion entre screen

--- app/src/main/java/yos/develop/recipeapp/core/navigation/NavGraphGlobal.kt
+++ app/src/main/java/yos/develop/recipeapp/core/navigation/NavGraphGlobal.kt
@@ -1,10 +1,14 @@
 package yos.develop.recipeapp.core.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -12,7 +16,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import yos.develop.recipeapp.core.utils.Catalog
 import yos.develop.recipeapp.screen.home.ui.HomeScreen
 import yos.develop.recipeapp.screen.home.ui.HomeViewModel
 import yos.develop.recipeapp.screen.login.ui.LoginScreen
@@ -25,71 +28,87 @@ fun NavGraphGlobal(
     navHostController: NavHostController,
     startDestination: Screens
 ) {
-    NavHost(
-        navController = navHostController,
-        startDestination = startDestination
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
     ) {
-        composable<Screens.EmptyScreen> {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.surface)
-            )
-        }
+        NavHost(
+            navController = navHostController,
+            startDestination = startDestination
+        ) {
+            composable<Screens.EmptyScreen> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.surface)
+                )
+            }
 
-        composable<Screens.LoginScreen> {
-            val viewModel = hiltViewModel<LoginViewModel>()
-            val state = viewModel.state
+            composable<Screens.LoginScreen> {
+                val viewModel = hiltViewModel<LoginViewModel>()
+                val state = viewModel.state
 
-            LoginScreen(
-                state = state,
-                onEvent = { event ->
-                    viewModel.onLoginEvent(event = event)
-                },
-                onNavigation = { screen ->
-                    navHostController.navigate(screen){
-                        popUpTo(0){
-                            inclusive = true
+                LoginScreen(
+                    state = state,
+                    onEvent = { event ->
+                        viewModel.onLoginEvent(event = event)
+                    },
+                    onNavigation = { screen ->
+                        navHostController.navigate(screen){
+                            popUpTo(0){
+                                inclusive = true
+                            }
                         }
                     }
-                }
-            )
-        }
+                )
+            }
 
-        composable<Screens.HomeScreen> {
-            val viewModel = hiltViewModel<HomeViewModel>()
-            val state = viewModel.state
+            composable<Screens.HomeScreen>(
+                enterTransition = { slideInVertically(initialOffsetY = { it }) + fadeIn(animationSpec = tween(300)) },
+                exitTransition = { slideOutVertically(targetOffsetY = { -it }) + fadeOut(animationSpec = tween(300)) },
+                popEnterTransition = { slideInVertically(initialOffsetY = { -it }) + fadeIn(animationSpec = tween(300)) },
+                popExitTransition = { slideOutVertically(targetOffsetY = { it }) + fadeOut(animationSpec = tween(300)) }
+            ) {
+                val viewModel = hiltViewModel<HomeViewModel>()
+                val state = viewModel.state
 
-            HomeScreen(
-                state = state,
-                onEvent = { event ->
-                    viewModel.onHomeEvent(event = event)
-                },
-                onGoLogin = {
-                    navHostController.popBackStack()
-                    navHostController.navigate(Screens.LoginScreen)
-                },
-                onNavigation = { screen ->
-                    navHostController.navigate(screen)
-                }
-            )
-        }
+                HomeScreen(
+                    state = state,
+                    onEvent = { event ->
+                        viewModel.onHomeEvent(event = event)
+                    },
+                    onGoLogin = {
+                        navHostController.popBackStack()
+                        navHostController.navigate(Screens.LoginScreen)
+                    },
+                    onNavigation = { screen ->
+                        navHostController.navigate(screen)
+                    }
+                )
+            }
 
-        composable<Screens.RecipeScreen> {
-            val recipeScreen: Screens.RecipeScreen = it.toRoute()
-            val viewModel = hiltViewModel<RecipeViewModel>()
-            val state = viewModel.state
+            composable<Screens.RecipeScreen>(
+                enterTransition = { slideInVertically(initialOffsetY = { it }) + fadeIn(animationSpec = tween(300)) },
+                exitTransition = { slideOutVertically(targetOffsetY = { -it }) + fadeOut(animationSpec = tween(300)) },
+                popEnterTransition = { slideInVertically(initialOffsetY = { -it }) + fadeIn(animationSpec = tween(300)) },
+                popExitTransition = { slideOutVertically(targetOffsetY = { it }) + fadeOut(animationSpec = tween(300)) }
+            ) {
+                val recipeScreen: Screens.RecipeScreen = it.toRoute()
+                val viewModel = hiltViewModel<RecipeViewModel>()
+                val state = viewModel.state
 
-            RecipeScreen(
-                state =  state,
-                idRecipe = recipeScreen.idRecipe,
-                onEvent = { event ->
-                    viewModel.onRecipeEvent(event = event)
-                },
-                onBack = {
-                    navHostController.navigateUp()
-                }
-            )
+                RecipeScreen(
+                    state =  state,
+                    idRecipe = recipeScreen.idRecipe,
+                    onEvent = { event ->
+                        viewModel.onRecipeEvent(event = event)
+                    },
+                    onBack = {
+                        navHostController.navigateUp()
+                    }
+                )
+            }
         }
     }
 }
